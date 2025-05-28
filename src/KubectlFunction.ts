da1f296b74ff6b49d014090b1060dc7b888b801b
Matthew Bonig <matthew.bonig@gmail.com>
2025-05-28 13:54:20 -0600

    chore: upgrading kubectl deps and support

--- src/KubectlFunction.ts
+++ src/KubectlFunction.ts
@@ -2,12 +2,12 @@ import * as path from 'path';
 
 import { KubectlV28Layer } from '@aws-cdk/lambda-layer-kubectl-v28';
 import { KubectlV30Layer } from '@aws-cdk/lambda-layer-kubectl-v30';
+import { KubectlV31Layer } from '@aws-cdk/lambda-layer-kubectl-v31';
 import { CfnOutput, Duration } from 'aws-cdk-lib';
 import { IVpc, SubnetType } from 'aws-cdk-lib/aws-ec2';
 import { Effect, PolicyStatement } from 'aws-cdk-lib/aws-iam';
-import { Code, Function, FunctionOptions, Runtime } from 'aws-cdk-lib/aws-lambda';
+import { Code, Function, FunctionOptions, LayerVersion, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { AwsCliLayer } from 'aws-cdk-lib/lambda-layer-awscli';
-import { KubectlLayer } from 'aws-cdk-lib/lambda-layer-kubectl';
 
 import { Construct } from 'constructs';
 
@@ -19,14 +19,24 @@ export interface KubectlFunctionProps {
    * Whether to use kubectl v1.30 or not for the lambda layer. Set this to true if your EKS cluster is 1.28+
    *
    * @default false
+   * @deprecated Use kubectlLayer instead
    */
   readonly usev130?: boolean;
   /**
    * Whether to use kubectl v1.28 or not for the lambda layer. Set this to true if your EKS cluster is 1.26+
    *
    * @default false
+   * @deprecated Use kubectlLayer instead
    */
   readonly usev128?: boolean;
+
+  /**
+   * The KubectlLayer version to use. Cannot be used in conjunction with usev128 or usev130
+   *
+   * @default KubectlV31Layer
+   */
+  readonly kubectlLayer?: LayerVersion;
+
   /**
    * The Role ARN that is to be assumed during the EKS authentication process to access and run commands on the cluster.
    */
@@ -82,12 +92,22 @@ export class KubectlFunction extends Construct {
     }));
 
     this.handler.addLayers(new AwsCliLayer(this, 'AwsCliLayer'));
+    if (props.usev128 && props.kubectlLayer) {
+      throw Error('You cannot specify both usev128 and kubectlLayer. usev128 is deprecated, please use kubectlLayer instead.');
+    }
+    if (props.usev130 && props.kubectlLayer) {
+      throw Error('You cannot specify both usev130 and kubectlLayer. usev130 is deprecated, please use kubectlLayer instead.');
+    }
+    if (props.kubectlLayer) {
+      this.handler.addLayers(props.kubectlLayer);
+    }
+
     if (props.usev128) {
       this.handler.addLayers(new KubectlV28Layer(this, 'KubectlLayer'));
     } else if (props.usev130) {
       this.handler.addLayers(new KubectlV30Layer(this, 'KubectlLayer'));
     } else {
-      this.handler.addLayers(new KubectlLayer(this, 'KubectlLayer'));
+      this.handler.addLayers(new KubectlV31Layer(this, 'KubectlLayer'));
     }
 
 
